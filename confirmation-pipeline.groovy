abad88c479aa578c0f5dccf28e877210af494ee5
wissensalt <wissensalt@gmail.com>
2020-09-14 09:02:39 +0700

    add confirmation pipeline

---
+++ confirmation-pipeline.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    agent any
+
+    environment {
+        RELEASE = '1.0.0.RELEASE'
+    } 
+
+    stages {
+        stage ('Build') {
+            agent any
+            environment {
+                LOG_LEVEL = 'INFO'
+            }
+            steps {
+                echo "Building release ${RELEASE} with log level ${LOG_LEVEL}"
+            }
+        }
+
+        stage ('Test') {
+            steps {
+                echo "Testing Release ${RELEASE}"
+            }
+        }
+
+        stage ('Deploy') {
+            input {
+                message 'Sure to deploy ?'
+                ok 'Do it !'
+                parameters {
+                    string(name: 'TARGET_ENVIRONMENT', defaultValue: 'PROD', description: 'Target deployment environment')
+                }
+            }
+            steps {
+                echo "Deploying relase ${RELEASE} to environment ${TARGET_ENVIRONMENT}"
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'Print message wether the deployment is successfull or failed'
+        }
+    }
+}
