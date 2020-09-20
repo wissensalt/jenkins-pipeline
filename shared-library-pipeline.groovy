2bd6dc39c4288d02718ade9a7df6f618c757a9bb
wissensalt <wissensalt@gmail.com>
2020-09-20 16:04:56 +0700

    add shared-library-pipeline

---
+++ shared-library-pipeline.groovy
@@ -0,0 +1,47 @@
+library identifier: 'jenkins-pipeline-library@develop',
+        retriever: modernSCM([$class: 'GitSCMSource', remote: 'https://github.com/wissensalt/jenkins-pipeline-library.git'])
+pipeline {
+    agent any 
+
+    parameters {
+        booleanParam(name: 'RC', defaultValue: false, description: 'Is this release candidate ?')
+    }
+
+    environment {
+        VERSION = '1.0.0'
+        VERSION_RC = 'RC.2'    
+    }
+
+    stages {
+        stage('Audit Tools') {
+            steps {
+                auditTools()
+            }            
+        }
+        stage('Build') {
+            environment {
+                VERSION_SUFFIX = getVersionSuffix()
+            }
+
+            steps {
+                echo "Building version : ${VERSION} with suffix ${VERSION_SUFFIX}"
+            }
+        }
+
+        stage('Test') {
+            steps {
+                    echo 'Conduct Testing'
+                }
+        }
+
+        stage('Publish') {
+            when {
+                expression { return params.RC }
+            }
+
+            steps {
+                echo "Complete Published"
+            }
+        }
+    }
+}
