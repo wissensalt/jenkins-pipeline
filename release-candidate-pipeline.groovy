8a30be9456b3cee11aa0067cdb7c36ca72df07f0
wissensalt <wissensalt@gmail.com>
2020-09-20 15:09:58 +0700

    fix script

--- release-candidate-pipeline.groovy
+++ release-candidate-pipeline.groovy
@@ -15,7 +15,6 @@ pipeline {
             steps {
                 sh '''
                     git version
-                    docker version
                 '''
             }            
         }
