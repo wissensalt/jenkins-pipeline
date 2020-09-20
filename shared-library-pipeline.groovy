faa072c115d58472c4d3ebd87c104a0c2f659d40
wissensalt <wissensalt@gmail.com>
2020-09-20 16:50:03 +0700

    fix script

--- shared-library-pipeline.groovy
+++ shared-library-pipeline.groovy
@@ -20,7 +20,7 @@ pipeline {
         }
         stage('Build') {
             environment {
-                VERSION_SUFFIX = getVersionSuffix()
+                VERSION_SUFFIX = versionSuffix()
             }
 
             steps {
